7eaad47f69cc4751552a33285b85e666c604eb00
AvinashBolleddula <avinash19961006@gmail.com>
2025-11-04 21:30:29 -0600

    Time: 150 ms (36.79%), Space: 32.2 MB (63.38%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -6,19 +6,28 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node,curr):
-            good  = 0
+        # we need to follow dfs here because the path is from root to node
+        # preorder dfs as we check if parent is good node and then enter children
+        def dfs(node, max_so_far):
+            # Base case root is Null
             if not node:
                 return 0
-            if node.left == None and node.right == None and node.val >= curr:
-                return 1
-            if node.val >= curr:
-                curr = node.val
-                good += 1
-            left = dfs(node.left,curr)
-            right = dfs(node.right,curr)
-
-            return left + right + good
-    
-        return dfs(root,root.val)
+            
+            # Check if current node is good
+            # Here good is local variable not global
+            good = 1 if node.val >= max_so_far else 0
+            
+            # Update max_so_far for children
+            # if parent is good, choose its val as max
+            # else stay as it is 
+            new_max = max(max_so_far, node.val)
+            
+            # Recurse and accumulate counts
+            left = dfs(node.left, new_max)
+            right = dfs(node.right, new_max)
+            
+            # do something on subtrees after recurrsion, depending on problem
+            return good + left + right
         
+        # start with -inf or rrot.val not 0 to counter neg values of root node
+        return dfs(root, float('-inf'))
