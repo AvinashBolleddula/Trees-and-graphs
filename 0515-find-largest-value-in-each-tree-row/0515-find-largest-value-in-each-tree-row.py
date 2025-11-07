5c39674a9429db43757196f2376a7b52847cc345
AvinashBolleddula <avinash19961006@gmail.com>
2025-11-06 18:56:28 -0600

    Time: 4 ms (19.81%), Space: 19.8 MB (86.13%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -6,16 +6,23 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        #Asking largest value in each row, so BFS
-        # implement BFS with queue in iterative manner
+        # Asking largest value in each row, so BFS
+        # Implement BFS with queue in iterative manner and at each level initialize max_val to -inf
+        # And while looping through that level, compare and get max_val of that level and add it to result array
+        
+        
+        
+        # Base case, when root is None, return []
         if not root:
             return []
         queue = deque([root])
+        # Array to capture max elements in each level
         large_arr = []
         while queue:
             nodes_in_current_level = len(queue)
+            # At start of each level initialize max_val to -inf
             max_val = float('-inf')
-
+            # Loop through each element in a level and get max val in that level and append it to result array
             for _ in range(nodes_in_current_level):
                 node  = queue.popleft()
                 max_val = max(max_val,node.val)
